6649e68344745bc03a45a6af6a71651d1f1b762f
student <student@MacBook-Pro-11.local>
2015-05-23 15:28:27 +0900

    0523

---
+++ Assets/Arm.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arm : MonoBehaviour {
+	public Vector2 SPEED = new Vector2(0.05f, 0.05f);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 Position = transform.position;
+		// 左キーを押し続けていたら
+		if(Input.GetKey("b")){
+			// 代入したPositionに対して加算減算を行う
+			Position.x -= SPEED.x;
+		} else if(Input.GetKey("v")){ // 右キーを押し続けていたら
+			// 代入したPositionに対して加算減算を行う
+			Position.x += SPEED.x;
+		}
+		// 現在の位置に加算減算を行ったPositionを代入する
+		transform.position = Position;
+	}
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	public const int SPEED = 5;
+
+	private void Update()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+		transform.Translate(Vector3.right * x * SPEED * Time.deltaTime);
+		transform.Translate(Vector3.forward * y * SPEED * Time.deltaTime);
+	}
+}
